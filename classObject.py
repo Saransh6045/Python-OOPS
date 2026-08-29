247578de6cea0856ffdb74bde23452174f213b83
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-08-29 17:27:56 +0530

    Class & Object

---
+++ classObject.py
@@ -0,0 +1,21 @@
+#creating a class
+class Student:
+
+    #special method/magic method/dunder method -> constructor to define data/attributes
+    def __init__(self):
+        print("Initializing the class object")
+        self.name = "Saransh"
+        self.id = 101
+        self.marks = 90
+        print("Object initialized successfully")
+
+    #Inializing a method
+    def displayPercentage(self, totalMarks):
+        percentage = self.marks / totalMarks * 100
+        print("Percentage of student is: ", percentage, "%")
+
+#Creating an object of the class
+student = Student()
+
+#Running the method manually
+student.displayPercentage(100)
